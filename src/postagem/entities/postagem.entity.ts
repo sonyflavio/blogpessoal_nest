e0181726ad110646d297be5ae558240b47166c3d
Flavio Serra <sonyflavio@gmail.com>
2025-09-23 07:30:05 -0300

    first commit

---
+++ src/postagem/entities/postagem.entity.ts
@@ -0,0 +1,27 @@
+import { IsNotEmpty } from "class-validator"
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+
+//dando nome a tabela do banco de dados
+@Entity({name: 'tb_postagens' })
+export class Postagem {
+    @PrimaryGeneratedColumn()
+    id: number
+    
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    titulo: string
+
+
+     @IsNotEmpty()
+    @Column({length: 1000, nullable: false})
+    texto: string
+
+    @UpdateDateColumn()
+    data: Date
+}
+
+
+// id PRIMARY KEY AUTO_INCREMENT
+// titulo varchar(100) NOT NULL
+// texto varchar(1000) NOT NULL
+// data DATE(6) UPDATED_TIMESTAMP
---
+++ src/postagem/postagem.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Postagem } from "./entities/postagem.entity";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Postagem])],
+    providers: [],
+    controllers: [],
+    exports: [],
+})
+
+export class PostagemModule {}
