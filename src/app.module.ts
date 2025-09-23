d155a251409aca737be7097ce3e5c86a2e84c13a
Flavio Serra <sonyflavio@gmail.com>
2025-09-23 08:02:07 -0300

    método consultar todas as Postagens

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { PostagemModule } from './postagem/postagem.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port:  3306,
+      port: 3306,
       username: 'root',
       password: 'root',
       database: 'db_blogpessoal',
---
+++ src/postagem/controllers/postagem.controller.ts
@@ -0,0 +1,15 @@
+import { Postagem } from '../entities/postagem.entity';
+import { PostagemService } from './../../services/postagem.service';
+import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+
+@Controller("/postagens")
+export class PostagemController {
+    constructor(private readonly PostagemService: PostagemService) {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Postagem[]> {
+        return this.PostagemService.findAll();
+    }
+
+}    
