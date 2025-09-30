ba0a8a5866ea9d71185391a168d5777a6e2c4141
Flavio Serra <sonyflavio@gmail.com>
2025-09-30 10:51:15 -0300

    Criando o Modulo Auth  - Parte 01

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.modules';
 import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { Tema } from './tema/entities/tema.entity';
@@ -19,6 +20,7 @@ import { TemaModule } from './tema/tema.module';
     }),
     PostagemModule,
     TemaModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
