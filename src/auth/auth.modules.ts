09e514d2d0d0eea1f4512dc68ec9299c4a0b80ae
Flavio Serra <sonyflavio@gmail.com>
2025-10-06 07:41:56 -0300

    Criando Módulo Auth - Parte 02

--- src/auth/auth.modules.ts
+++ src/auth/auth.modules.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsuarioModule } from '../usuario/usuario.module';
 import { Bcrypt } from './bcrypt/bcrypt';
+import { jwtConstants } from './constants/constants';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
-  imports: [],
-  providers: [Bcrypt],
-  controllers: [],
+  imports: [
+    forwardRef(() => UsuarioModule),
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [Bcrypt, AuthService, LocalStrategy],
   exports: [Bcrypt],
 })
 export class AuthModule {}
---
+++ src/auth/constants/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'Wcd8Vvz81V6XWxlVdibc0jQ3hTVahkNaSLBrV6EgNF1hShHML9xFKa5nLsD4lwb7',
+};
